4eab49173e9475c57721ff347c01ec8814f56cd3
王英豪 <wangyinghao_sx@qiyi.com>
2020-05-19 21:49:20 +0800

    2020-5-19

--- coding-interviews/src/_2/_9/Node.java
+++ coding-interviews/src/_2/_9/Node.java
@@ -1,9 +1,66 @@
 package _2._9;
 
+import java.util.HashMap;
+
 /**
  * @Description: 复制含有随机指针节点的链表
  * @Author: wangyinghao_sx
  * @Date: 2020-05-19 20:06
  **/
 public class Node {
+    public int value;
+    public Node next;
+    public Node rand;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node copyListWithRand1(Node head) {
+        HashMap<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.value));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).rand = map.get(cur.rand);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+
+    public Node copyListWithRand2(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        Node next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = new Node(cur.value);
+            cur.next.next = next;
+            cur = next;
+        }
+        cur = head;
+        Node curCopy = null;
+        while (cur != null) {
+            next = cur.next.next;
+            curCopy = cur.next;
+            curCopy.rand = cur.rand != null ? cur.rand.next : null;
+            cur = next;
+        }
+        Node res = head.next;
+        cur = head;
+        while (cur != null) {
+            next = cur.next.next;
+            curCopy = cur.next;
+            cur.next = next;
+            curCopy.next = next != null ? next.next : null;
+            cur = next;
+        }
+        return res;
+    }
 }
